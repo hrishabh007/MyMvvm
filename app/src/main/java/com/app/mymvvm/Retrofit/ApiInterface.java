318fc04705fcf2d42ecd5dae2e0e7bd87b456ffe
hrish <hrishabh.balani11@gmail.com>
2020-09-24 14:35:49 +0530

    name change

--- app/src/main/java/com/app/mymvvm/Retrofit/ApiInterface.java
+++ app/src/main/java/com/app/mymvvm/Retrofit/ApiInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 public interface ApiInterface {
 
     String CACHE = "Cache-Control: max-age=0";
-    String AGENT = "Data-Agent: Android News App";
+    String AGENT = "Data-Agent: MvvmApp";
 
     @GET("movie/popular")
     Call<MoviewResponse> getPopularMovies(@Query("api_key") String apiKey);
